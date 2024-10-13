939c7e66e5616141f5c1037f43f796cd5d6998d3
milson6389 <audie.milson38@gmail.com>
2024-10-13 11:20:15 +0700

    Testing Netlify Express

--- src/app/web.js
+++ src/app/web.js
@@ -1,14 +1,21 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import serverless from "serverless-http";
 import { corsOptions } from "../config/corsOptions.js";
 import { errorMiddleware } from "../middleware/errorMiddleware.js";
 import { courseRouter } from "../routes/courseApi.js";
 
 dotenv.config();
 
-export const web = express();
+const PORT = process.env.PORT || 8000;
+const web = express();
 web.use(cors(corsOptions));
 web.use(express.json());
 web.use(courseRouter);
 web.use(errorMiddleware);
+web.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+export const handler = serverless(web);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,1 @@
-import { web } from "./app/web.js";
-
-const PORT = process.env.PORT || 8000;
-web.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+import { handler } from "./app/web.js";
