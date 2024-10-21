b7298f3867f099a4a36c4c53745ebf9150c27afc
milson6389 <audie.milson38@gmail.com>
2024-10-22 01:31:59 +0700

    Feat: Move JSON Data

--- src/service/courseService.js
+++ src/service/courseService.js
@@ -1,5 +1,636 @@
 import { ResponseError } from "../error/responseError.js";
-import DataKelas from "../data/kelas.json" assert { type: "json" };
+
+const DataKelas = [
+  {
+    id: 1,
+    lecturerName: "Jenna Ortega",
+    lecturerTitle: "Senior Market Analyst",
+    lecturerDesc:
+      "Berkarier di bidang Marketing selama lebih dari 3 tahun. Saat ini bekerja sebagai Senior Market Analyst di Startup selama hampir 1 tahun",
+    thumbnail:
+      "https://images.unsplash.com/photo-1501523460185-2aa5d2a0f981?q=80&w=1831&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Marketing: 101",
+    desc: "Mulai transformasi dengan instruktur profesional, harga yang terjangkau, dan...",
+    category: "Pemasaran",
+    rating: 3,
+    price: 300,
+    materials: [
+      {
+        id: 1,
+        title: "Introduction Course",
+        sub_material: [
+          {
+            title: "Intro 1",
+            duration: 12,
+          },
+          {
+            title: "Intro 2",
+            duration: 8,
+          },
+          {
+            title: "Intro 3",
+            duration: 20,
+          },
+        ],
+      },
+      {
+        id: 2,
+        title: "Basic Course",
+        sub_material: [
+          {
+            title: "Basic 1",
+            duration: 12,
+          },
+          {
+            title: "Basic 2",
+            duration: 8,
+          },
+        ],
+      },
+      {
+        id: 3,
+        title: "Advance Course",
+        sub_material: [
+          {
+            title: "Advance 1",
+            duration: 12,
+          },
+          {
+            title: "Advance 2",
+            duration: 8,
+          },
+          {
+            title: "Advance 3",
+            duration: 20,
+          },
+          {
+            title: "Advance 4",
+            duration: 5,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    lecturerName: "Ortega",
+    lecturerTitle: "Accountant",
+    lecturerDesc:
+      "Berkarier di bidang Marketing selama lebih dari 3 tahun. Saat ini bekerja sebagai Accountant di Startup selama hampir 1 tahun",
+    thumbnail:
+      "https://images.unsplash.com/photo-1510247548804-1a5c6f550b2d?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Market Research Guide",
+    desc: "Mulai transformasi dengan instruktur profesional, harga yang terjangkau, dan...",
+    category: "Pemasaran",
+    rating: 4,
+    price: 250,
+    materials: [
+      {
+        id: 1,
+        title: "Introduction Course",
+        sub_material: [
+          {
+            title: "Intro 1",
+            duration: 12,
+          },
+          {
+            title: "Intro 2",
+            duration: 8,
+          },
+          {
+            title: "Intro 3",
+            duration: 20,
+          },
+        ],
+      },
+      {
+        id: 2,
+        title: "Basic Course",
+        sub_material: [
+          {
+            title: "Basic 1",
+            duration: 12,
+          },
+          {
+            title: "Basic 2",
+            duration: 8,
+          },
+        ],
+      },
+      {
+        id: 3,
+        title: "Advance Course",
+        sub_material: [
+          {
+            title: "Advance 1",
+            duration: 12,
+          },
+          {
+            title: "Advance 2",
+            duration: 8,
+          },
+          {
+            title: "Advance 3",
+            duration: 20,
+          },
+          {
+            title: "Advance 4",
+            duration: 5,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 3,
+    lecturerName: "Jenna Ortega",
+    lecturerTitle: "Creative Designer Team Lead",
+    lecturerDesc:
+      "Berkarier di bidang Designer selama lebih dari 3 tahun. Saat ini bekerja sebagai Creative Designer Team Lead di Startup selama hampir 1 tahun",
+    thumbnail:
+      "https://images.unsplash.com/photo-1484788984921-03950022c9ef?q=80&w=1832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "UI/UX Design 101",
+    desc: "Mulai transformasi dengan instruktur profesional, harga yang terjangkau, dan...",
+    category: "Desain",
+    rating: 4,
+    price: 100,
+    materials: [
+      {
+        id: 1,
+        title: "Introduction Course",
+        sub_material: [
+          {
+            title: "Intro 1",
+            duration: 12,
+          },
+          {
+            title: "Intro 2",
+            duration: 8,
+          },
+          {
+            title: "Intro 3",
+            duration: 20,
+          },
+        ],
+      },
+      {
+        id: 2,
+        title: "Basic Course",
+        sub_material: [
+          {
+            title: "Basic 1",
+            duration: 12,
+          },
+          {
+            title: "Basic 2",
+            duration: 8,
+          },
+        ],
+      },
+      {
+        id: 3,
+        title: "Advance Course",
+        sub_material: [
+          {
+            title: "Advance 1",
+            duration: 12,
+          },
+          {
+            title: "Advance 2",
+            duration: 8,
+          },
+          {
+            title: "Advance 3",
+            duration: 20,
+          },
+          {
+            title: "Advance 4",
+            duration: 5,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 4,
+    lecturerName: "Jenna Ortega",
+    lecturerTitle: "CEO Startup",
+    lecturerDesc: "Saat ini bekerja sebagai ceo dari sebuah startup yang telah beroperasi selama 3 tahun",
+    thumbnail:
+      "https://images.unsplash.com/photo-1493946740644-2d8a1f1a6aff?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Business Administration",
+    desc: "Mulai transformasi dengan instruktur profesional, harga yang terjangkau, dan...",
+    category: "Bisnis",
+    rating: 2,
+    price: 300,
+    materials: [
+      {
+        id: 1,
+        title: "Introduction Course",
+        sub_material: [
+          {
+            title: "Intro 1",
+            duration: 12,
+          },
+          {
+            title: "Intro 2",
+            duration: 8,
+          },
+          {
+            title: "Intro 3",
+            duration: 20,
+          },
+        ],
+      },
+      {
+        id: 2,
+        title: "Basic Course",
+        sub_material: [
+          {
+            title: "Basic 1",
+            duration: 12,
+          },
+          {
+            title: "Basic 2",
+            duration: 8,
+          },
+        ],
+      },
+      {
+        id: 3,
+        title: "Advance Course",
+        sub_material: [
+          {
+            title: "Advance 1",
+            duration: 12,
+          },
+          {
+            title: "Advance 2",
+            duration: 8,
+          },
+          {
+            title: "Advance 3",
+            duration: 20,
+          },
+          {
+            title: "Advance 4",
+            duration: 5,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 5,
+    lecturerName: "Jenna Ortega",
+    lecturerTitle: "Certified ZEN",
+    lecturerDesc:
+      "Berkarier di bidang Self Development dan bertujuan untuk membagikan cara cara mencapai kehidupan yang ZEN",
+    thumbnail:
+      "https://images.unsplash.com/photo-1499728603263-13726abce5fd?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Inner Peace",
+    desc: "Mulai transformasi dengan instruktur profesional, harga yang terjangkau, dan...",
+    category: "Development",
+    rating: 3,
+    price: 175,
+    materials: [
+      {
+        id: 1,
+        title: "Introduction Course",
+        sub_material: [
+          {
+            title: "Intro 1",
+            duration: 12,
+          },
+          {
+            title: "Intro 2",
+            duration: 8,
+          },
+          {
+            title: "Intro 3",
+            duration: 20,
+          },
+        ],
+      },
+      {
+        id: 2,
+        title: "Basic Course",
+        sub_material: [
+          {
+            title: "Basic 1",
+            duration: 12,
+          },
+          {
+            title: "Basic 2",
+            duration: 8,
+          },
+        ],
+      },
+      {
+        id: 3,
+        title: "Advance Course",
+        sub_material: [
+          {
+            title: "Advance 1",
+            duration: 12,
+          },
+          {
+            title: "Advance 2",
+            duration: 8,
+          },
+          {
+            title: "Advance 3",
+            duration: 20,
+          },
+          {
+            title: "Advance 4",
+            duration: 5,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 6,
+    lecturerName: "Jenna Ortega",
+    lecturerTitle: "Certified ZEN",
+    lecturerDesc:
+      "Berkarier di bidang Self Development dan bertujuan untuk membagikan cara cara mencapai kehidupan yang ZEN",
+    thumbnail:
+      "https://plus.unsplash.com/premium_photo-1666946131242-b2c5cc73892a?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Balance: 101",
+    desc: "Mulai transformasi dengan instruktur profesional, harga yang terjangkau, dan...",
+    category: "Development",
+    rating: 2,
+    price: 200,
+    materials: [
+      {
+        id: 1,
+        title: "Introduction Course",
+        sub_material: [
+          {
+            title: "Intro 1",
+            duration: 12,
+          },
+          {
+            title: "Intro 2",
+            duration: 8,
+          },
+          {
+            title: "Intro 3",
+            duration: 20,
+          },
+        ],
+      },
+      {
+        id: 2,
+        title: "Basic Course",
+        sub_material: [
+          {
+            title: "Basic 1",
+            duration: 12,
+          },
+          {
+            title: "Basic 2",
+            duration: 8,
+          },
+        ],
+      },
+      {
+        id: 3,
+        title: "Advance Course",
+        sub_material: [
+          {
+            title: "Advance 1",
+            duration: 12,
+          },
+          {
+            title: "Advance 2",
+            duration: 8,
+          },
+          {
+            title: "Advance 3",
+            duration: 20,
+          },
+          {
+            title: "Advance 4",
+            duration: 5,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 7,
+    lecturerName: "Jenna Ortega",
+    lecturerTitle: "Senior Technical Analyst",
+    lecturerDesc:
+      "Berkarier di Financial Market selama lebih dari 3 tahun. Saat ini bekerja sebagai full time trader dan technical analyst di sekuritas selama hampir 2 tahun",
+    thumbnail:
+      "https://plus.unsplash.com/premium_photo-1681487767138-ddf2d67b35c1?q=80&w=1855&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Understanding Financial Market",
+    desc: "Mulai transformasi dengan instruktur profesional, harga yang terjangkau, dan...",
+    category: "Bisnis",
+    rating: 5,
+    price: 400,
+    materials: [
+      {
+        id: 1,
+        title: "Introduction Course",
+        sub_material: [
+          {
+            title: "Intro 1",
+            duration: 12,
+          },
+          {
+            title: "Intro 2",
+            duration: 8,
+          },
+          {
+            title: "Intro 3",
+            duration: 20,
+          },
+        ],
+      },
+      {
+        id: 2,
+        title: "Basic Course",
+        sub_material: [
+          {
+            title: "Basic 1",
+            duration: 12,
+          },
+          {
+            title: "Basic 2",
+            duration: 8,
+          },
+        ],
+      },
+      {
+        id: 3,
+        title: "Advance Course",
+        sub_material: [
+          {
+            title: "Advance 1",
+            duration: 12,
+          },
+          {
+            title: "Advance 2",
+            duration: 8,
+          },
+          {
+            title: "Advance 3",
+            duration: 20,
+          },
+          {
+            title: "Advance 4",
+            duration: 5,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 8,
+    lecturerName: "Jenna Ortega",
+    lecturerTitle: "Adobe Certified Designer",
+    lecturerDesc:
+      "Berkarier di bidang Designer selama lebih dari 3 tahun. Saat ini bekerja sebagai Adobe Certified Designer di Startup selama hampir 1 tahun",
+    thumbnail:
+      "https://plus.unsplash.com/premium_photo-1661382011487-cd3d6b1d9dff?q=80&w=1742&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "XD Design Certification",
+    desc: "Mulai transformasi dengan instruktur profesional, harga yang terjangkau, dan...",
+    category: "Desain",
+    rating: 3,
+    price: 500,
+    materials: [
+      {
+        id: 1,
+        title: "Introduction Course",
+        sub_material: [
+          {
+            title: "Intro 1",
+            duration: 12,
+          },
+          {
+            title: "Intro 2",
+            duration: 8,
+          },
+          {
+            title: "Intro 3",
+            duration: 20,
+          },
+        ],
+      },
+      {
+        id: 2,
+        title: "Basic Course",
+        sub_material: [
+          {
+            title: "Basic 1",
+            duration: 12,
+          },
+          {
+            title: "Basic 2",
+            duration: 8,
+          },
+        ],
+      },
+      {
+        id: 3,
+        title: "Advance Course",
+        sub_material: [
+          {
+            title: "Advance 1",
+            duration: 12,
+          },
+          {
+            title: "Advance 2",
+            duration: 8,
+          },
+          {
+            title: "Advance 3",
+            duration: 20,
+          },
+          {
+            title: "Advance 4",
+            duration: 5,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 9,
+    lecturerName: "Jenna Ortega",
+    lecturerTitle: "Market Research Lead",
+    lecturerDesc:
+      "Berkarier di bidang Marketing selama lebih dari 3 tahun. Saat ini bekerja sebagai Market Research Lead di Startup selama hampir 1 tahun",
+    thumbnail:
+      "https://images.unsplash.com/photo-1607703703520-bb638e84caf2?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Marketing",
+    desc: "Mulai transformasi dengan instruktur profesional, harga yang terjangkau, dan...",
+    category: "Pemasaran",
+    rating: 3,
+    price: 85,
+    materials: [
+      {
+        id: 1,
+        title: "Introduction Course",
+        sub_material: [
+          {
+            title: "Intro 1",
+            duration: 12,
+          },
+          {
+            title: "Intro 2",
+            duration: 8,
+          },
+          {
+            title: "Intro 3",
+            duration: 20,
+          },
+        ],
+      },
+      {
+        id: 2,
+        title: "Basic Course",
+        sub_material: [
+          {
+            title: "Basic 1",
+            duration: 12,
+          },
+          {
+            title: "Basic 2",
+            duration: 8,
+          },
+        ],
+      },
+      {
+        id: 3,
+        title: "Advance Course",
+        sub_material: [
+          {
+            title: "Advance 1",
+            duration: 12,
+          },
+          {
+            title: "Advance 2",
+            duration: 8,
+          },
+          {
+            title: "Advance 3",
+            duration: 20,
+          },
+          {
+            title: "Advance 4",
+            duration: 5,
+          },
+        ],
+      },
+    ],
+  },
+];
 
 const paidCourse = [];
 
--- src/service/paymentService.js
+++ src/service/paymentService.js
@@ -1,5 +1,132 @@
 import { ResponseError } from "../error/responseError.js";
-import DataPayment from "../data/wop.json" assert { type: "json" };
+
+const DataPayment = [
+  {
+    id: 1,
+    title: "Transfer Bank",
+    trxType: "trf",
+    sub: [
+      {
+        title: "Bank BCA",
+        code: "bbca",
+        trxType: "trf",
+        va_code: "11111",
+        admin: 5000,
+        img: "https://i.pinimg.com/originals/29/61/0b/29610b7dbf7e4ea5070626923a12cba8.png",
+        isMaintenance: false,
+      },
+      {
+        title: "Bank BNI",
+        code: "bbni",
+        trxType: "trf",
+        va_code: "22222",
+        admin: 5000,
+        img: "https://i.pinimg.com/originals/36/38/43/36384348ef9d7bfff66da6da9e975d56.png",
+        isMaintenance: false,
+      },
+      {
+        title: "Bank BRI",
+        code: "bbri",
+        trxType: "trf",
+        va_code: "33333",
+        admin: 7000,
+        img: "https://i.pinimg.com/originals/6b/13/7a/6b137a6a174c2f054904a3be35fe249f.png",
+        isMaintenance: true,
+      },
+      {
+        title: "Bank Mandiri",
+        code: "bmri",
+        trxType: "trf",
+        va_code: "44444",
+        admin: 6500,
+        img: "https://vectorez.biz.id/wp-content/uploads/2023/10/Logo-Bank-Mandiri.png",
+        isMaintenance: true,
+      },
+    ],
+    guide: [
+      {
+        title: "Transfer via ATM",
+        desc: [
+          "1. Masukkan kartu ATM dan PIN Anda",
+          "2. Di menu utama pilih Transaksi Lainnya. Pilih Transfer. Pilih Ke Virtual Account",
+          "3. Masukkan nomor Virtual Account",
+          "4. Pastikan data Virtual Account Anda benar, kemudian masukkan angka yang perlu anda bayarkan, kemudian pilih Benar",
+          "5. Cek dan perhatikan konfirmasi pembayaran dari layar ATM, jika sudah benar pilih Ya, atau pilih Tidak jika data di layar masih salah",
+          "6. Transaksi Anda sudah selesai. Pilih Tidak untuk tidak melanjutkan transaksi lain",
+        ],
+      },
+      {
+        title: "Transfer via Mobile Banking",
+        desc: [
+          "1. Buka Aplikasi Mobile Banking",
+          "2. Di menu utama pilih Transaksi Lainnya. Pilih Transfer. Pilih Ke Virtual Account",
+          "3. Masukkan nomor Virtual Account",
+          "4. Pastikan data Virtual Account Anda benar, kemudian masukkan angka yang perlu anda bayarkan, kemudian Klik tombol Send",
+          "5. Klik OK untuk melanjutkan pembayaran",
+          "6. Masukkan PIN Anda untuk mengotorisasi transaksi",
+          "7. Transaksi Anda telah selesai",
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "E-Wallet",
+    trxType: "ewallet",
+    sub: [
+      {
+        title: "Dana",
+        code: "dana",
+        trxType: "ewallet",
+        va_code: "55555",
+        admin: 0,
+        img: "https://i.pinimg.com/originals/f5/8c/a3/f58ca3528b238877e9855fcac1daa328.jpg",
+        isMaintenance: false,
+      },
+      {
+        title: "OVO",
+        code: "ovo",
+        trxType: "ewallet",
+        va_code: "66666",
+        admin: 2500,
+        img: "https://static.vecteezy.com/system/resources/previews/028/766/360/original/ovo-ewallet-payment-icon-symbol-free-png.png",
+        isMaintenance: false,
+      },
+      {
+        title: "Link Aja",
+        code: "linkaja",
+        trxType: "ewallet",
+        va_code: "77777",
+        admin: 2000,
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/LinkAja.svg/2048px-LinkAja.svg.png",
+        isMaintenance: true,
+      },
+      {
+        title: "Shopee Pay",
+        code: "spay",
+        trxType: "ewallet",
+        va_code: "88888",
+        admin: 0,
+        img: "https://i.pinimg.com/736x/d0/19/16/d019163d861908ed0046391ebfa42ce1.jpg",
+        isMaintenance: false,
+      },
+    ],
+    guide: [
+      {
+        title: "Pembayaran via E-Wallet",
+        desc: [
+          "1. Buka Aplikasi E-Wallet",
+          "2. Pilih Transfer, kemudian pilih ke Rekening Bank",
+          "3. Masukkan nomor virtual account tujuan",
+          "4. Masukkan angka yang perlu anda bayarkan, kemudian pilih lanjutkan",
+          "5. Cek dan perhatikan detail konfirmasi pembayaran",
+          "6. Masukkan Pin E-Wallet / Biometric anda untuk mengkonfirmasi transfer",
+          "7. Transaksi Anda sudah selesai.",
+        ],
+      },
+    ],
+  },
+];
 
 const getAllPaymentGateways = () => {
   return DataPayment;
